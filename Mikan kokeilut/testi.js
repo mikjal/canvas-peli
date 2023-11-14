ac3866a339e80cc19cc24f881501fc01c4be5443
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2023-11-14 13:06:42 +0200

    Taustat + hahmo

--- Mikan kokeilut/testi.js
+++ Mikan kokeilut/testi.js
@@ -1,6 +1,53 @@
 const canvas = document.querySelector('#kanvas');
 const ctx = canvas.getContext("2d");
-let taustat = [], nopeus = 4;
+let taustat = [], vanha = 0;
+
+class Pelaaja {
+    constructor(kuvatiedosto) {
+        this.kuva = new Image();
+        this.frameja = 30;
+        this.nykyinenFrame = 0;
+        this.yOffset = 25;
+        this.xOffset = 50;
+        
+        this.kuva.onload = () => {
+            this.leveys = this.kuva.width / this.frameja;
+            this.korkeus = this.kuva.height;
+            this.paikka = {
+                x: canvas.width / 2 - this.leveys / 2 + this.xOffset,
+                y: canvas.height-this.korkeus-this.yOffset
+            }
+    
+        }
+        this.kuva.src = kuvatiedosto;
+        this.nopeus = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    piirra() {
+        ctx.drawImage(this.kuva,
+            this.nykyinenFrame*this.leveys,
+            0,
+            this.leveys,
+            this.korkeus,
+            this.paikka.x,
+            this.paikka.y,
+            this.leveys,
+            this.korkeus);
+        this.nykyinenFrame = (this.nykyinenFrame < this.frameja-1) ? this.nykyinenFrame += 1 : 0;
+    }
+
+    paivita() {
+        
+        this.piirra();
+
+    }
+}
+
+const pelaaja = new Pelaaja('flatboy-idle.png');
+
 
 class Tausta {
     constructor(kuvatiedosto, yPaikka, kerroin) { 
@@ -19,7 +66,7 @@ class Tausta {
         this.kerroin = kerroin; /* nopeuskerroin verrattuna pelaajan nopeuteen */
     }
 
-    piirra() {
+    piirra(nopeus) {
         ctx.drawImage(this.kuva,this.x,this.y);
         /* vähennetään x-paikasta nopeus*kerroin --> kuva siirtyy vasemmalle */
         this.x -= nopeus*this.kerroin;
@@ -37,7 +84,7 @@ class Tausta {
 
 }
 
-/* luodaan Tausta-luokan mukaiset oliot kaikille taustoille */
+/* luodaan Tausta-luokan mukaiset oliot kaikille taustoille, piirretään järjestyksessä ensimmäisestä viimeiseen */
 taustat = [ 
     /* Taustan parametrit: tiedostonimi, y-paikka, nopeuskerroin suhteessa pelihahmon nopeuteen*/
     new Tausta('kerros0-1024x288.png',0,0), /* y-paikka = 0 (yläreuna), nopeus = 0 (ei liiku) */
@@ -49,18 +96,16 @@ taustat = [
     new Tausta('kerros4-1920x25.png',-canvas.height,1.1)
 ];
 
+
 /* odotetaan että sivu on latautunut */
 window.onload = () => {
 
     taustat.forEach(tausta => {
         if (!tausta.latautunut) console.log(tausta.kuvatiedosto+' ei ole latautumut!');
     })
-
     
     animoi();
 }
-
-let vanha = 0;
 
 function animoi(aika) {
     window.requestAnimationFrame(animoi);
@@ -75,10 +120,18 @@ function animoi(aika) {
             ctx.fillStyle = 'skyblue';
             ctx.fillRect(0,0,canvas.width,canvas.height);
            
-            /* piirretään kaikki taustat siinä järjestyksessä kuin ne ovat arrayssa */
-            taustat.forEach(tausta => {
-                tausta.piirra();
-            });
+            let nopeus = pelaaja.nopeus.x;
+
+            /* piirretään kaikki taustat (paitsi näytöllä alin) siinä järjestyksessä kuin ne ovat arrayssa */
+            for (let i=0;i<taustat.length-1;i++) {
+                taustat[i].piirra(nopeus);
+            };
+
+            /* piirretään ja päivitetään pelaajan hahmo */
+            pelaaja.paivita();
+
+            /* piirretään alin tausta */
+            taustat[taustat.length-1].piirra(nopeus);
         }
 
     }
