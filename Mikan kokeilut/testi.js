0939524f8cc145be12cde7c96cf9e5ee04f66fea
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2023-11-17 13:15:42 +0200

    Toiminnassa: hyppy, debug, hitbox (aidan koodi puuttuu)

--- Mikan kokeilut/testi.js
+++ Mikan kokeilut/testi.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = true;
 ctx.imageSmoothingQuality = 'medium';
 
-let taustat = [], vanha = 0;
+let taustat = [], vanha = 0, painovoima = 0.5;
 
 class Pelaaja {
     constructor(kuvatiedosto, framejakuvassa) {
@@ -14,7 +14,33 @@ class Pelaaja {
         this.nykyinenFrame = 0;
         this.yOffsets = [2,0,0,0,2];
         this.xOffset = 50;
-        this.kuvarivi = 0;
+        this.kuvarivi = 0; // Mitä animaatio-"riviä" käytetään
+        this.hitbox = [ 
+                { // paikallaan
+                    alkaa: 0,
+                    paattyy: 0
+                },
+                { // kävelee
+                    alkaa: 4,
+                    paattyy: 85
+                },
+                { // juoksee
+                    alkaa: 10,
+                    paattyy: 105
+                },
+                { // hyppää
+                    alkaa: 2,
+                    paattyy: 96
+                },
+                { // kaatuu
+                    alkaa: 0,
+                    paattyy: 0
+                }
+            ]   
+        //    0,85,105,96,0
+        this.lakipiste = false;
+        this.aidanTakana = false;
+        this.vaihdetaanPuolta = false;
         this.kuva.onload = () => {
             this.leveys = this.kuva.width / this.kuvanframet;
             this.korkeus = this.kuva.height / 5;
@@ -25,6 +51,10 @@ class Pelaaja {
         }
         this.kuva.src = kuvatiedosto;
         this.nopeus = {
+            x: 0,
+            y: 0
+        }
+        this.paikka = {
             x: 0,
             y: 0
         }
@@ -41,19 +71,48 @@ class Pelaaja {
             this.leveys,
             this.korkeus,
             this.piirtopaikka.x,
-            this.piirtopaikka.y-this.yOffsets[this.kuvarivi],
+            this.piirtopaikka.y-this.yOffsets[this.kuvarivi]+this.paikka.y,
             this.leveys,
             this.korkeus
         );
 
         this.nykyinenFrame = (this.nykyinenFrame < this.haluttufps-1) ? this.nykyinenFrame += 1 : this.nykyinenFrame = 0;
 
+        if (this.hitbox[this.kuvarivi].paattyy != 0) {
+           ctx.globalAlpha = 0.2;
+           ctx.fillStyle = 'red';
+           ctx.fillRect(
+            this.piirtopaikka.x+this.hitbox[this.kuvarivi].alkaa,
+            this.piirtopaikka.y-this.yOffsets[this.kuvarivi]+this.paikka.y,
+            this.hitbox[this.kuvarivi].paattyy-this.hitbox[this.kuvarivi].alkaa,
+            this.korkeus
+           );
+           ctx.globalAlpha = 1.0;
+        }
     }
 
     paivita() {
         
         this.piirra();
 
+        // Nopeuden vaikutus
+        this.paikka.x += this.nopeus.x; 
+        this.paikka.y += this.nopeus.y;
+
+        // Nopeus y-suunnassa muuttuu painovoiman verran, jos pelaajan y on jotain muuta kuin nolla
+        let edellinen = this.nopeus.y;
+        this.nopeus.y = (this.paikka.y != 0) ? this.nopeus.y+painovoima : 0;
+        // Lakipiste?
+        if (edellinen < 0 && this.nopeus.y >= 0) {
+            this.lakipiste = true;
+            // Vaihdetaanko puolta?
+            this.aidanTakana = (this.vaihdetaanPuolta == true) ? !this.aidanTakana : this.aidanTakana;
+        }
+        // Ollaanko "maan" tasolla?
+        if (this.nopeus.y == 0 && this.paikka.y == 0) {
+            this.vaihdetaanPuolta = false;
+            this.lakipiste = false;
+        }
     }
 }
 
@@ -120,20 +179,33 @@ window.onload = () => {
 
     window.addEventListener('keydown', (evnt) => {
         if (evnt.key == 'ArrowUp' || evnt.code == 'ArrowUp') {
+            pelaaja.nopeus.y = -8;
+            // vaihdetaan puolta jos pelaaja ei ole aidan takana, muussa tapauksessa ei vaihdeta puolta
+            pelaaja.vaihdetaanPuolta = (pelaaja.aidanTakana == false) ? true : false;
+        }
+
+        if (evnt.key == 'ArrowDown' || evnt.code == 'ArrowDown') {
+            pelaaja.nopeus.y = -8;
+            // vaihdetaan puolta jos pelaaja on aidan takana, muussa tapauksessa ei vaihdeta puolta
+            pelaaja.vaihdetaanPuolta = (pelaaja.aidanTakana == true) ? true : false;
+        }
+
+        if (evnt.key == 'ArrowRight' || evnt.code == 'ArrowRight') {
             if (pelaaja.kuvarivi < 4) {
                 pelaaja.kuvarivi += 1;
                 pelaaja.nopeus.x = nopeudet[pelaaja.kuvarivi];
                 if (pelaaja.kuvarivi > 2) pelaaja.nykyinenFrame = 0;
             }
-            
         }
-        if (evnt.key == 'ArrowDown' || evnt.code == 'ArrowDown') {
+
+        if (evnt.key == 'ArrowLeft' || evnt.code == 'ArrowLeft') {
             if (pelaaja.kuvarivi > 0) {
                 pelaaja.kuvarivi -= 1;
                 pelaaja.nopeus.x = nopeudet[pelaaja.kuvarivi];
                 if (pelaaja.kuvarivi > 2) pelaaja.nykyinenFrame = 0;
             }
         }
+
     });
 
 
@@ -146,6 +218,8 @@ function animoi(aika) {
     fps = Math.round(1000 / (aika-vanha));
     if (Number.isInteger(fps)) {
         if (fps <= 30) { /* max. 30 fps */
+
+            // Debug
             document.querySelector('#ftime').innerText = fps;
             vanha = aika;
             
@@ -167,8 +241,15 @@ function animoi(aika) {
             taustat[taustat.length-1].piirra(nopeus);
 
             ctx.fillStyle = 'black';
-            ctx.font = '20px Arial';
-            ctx.fillText('Animaatiotesti (nuoli ylös/alas vaihtaa animaatiota)',10,25);
+            ctx.font = '16px Arial';
+            ctx.fillText('Nuoli vasemmalla/oikealle vaihtaa animaatiota, nuoli ylös/alas hyppää, ei aitaa, punainen alue = "hitbox"',10,25);
+
+            // Debug
+            document.querySelector('#paikka').innerText = pelaaja.paikka.x + ', ' +pelaaja.paikka.y;
+            document.querySelector('#nopeus').innerText = pelaaja.nopeus.x + ', ' +pelaaja.nopeus.y;
+            document.querySelector('#vpuoli').innerText = (pelaaja.vaihdetaanPuolta == false) ? 'Ei' : 'Kyllä';
+            document.querySelector('#lakipiste').innerText = (pelaaja.lakipiste == false) ? 'Ei' : 'Kyllä';
+            document.querySelector('#takana').innerText = (pelaaja.aidanTakana == false) ? 'Ei' : 'Kyllä';
 
         }
 
