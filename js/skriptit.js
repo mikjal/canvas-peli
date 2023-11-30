a274b1f02e6950950aa93e8e37fd0f3673e83b16
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2023-11-30 10:25:33 +0200

    Alustukset

--- js/skriptit.js
+++ js/skriptit.js
@@ -1,3 +1,34 @@
-/* täällä ei ole vielä mitään */
-// Kun haluat näyttää viestin
-document.getElementById('odota').style.display = 'block';
+let vanhaAika = 0; // Ruudunpäivityksen ajastukseen
+
+// Tautakuvat
+const taustakuvat = new Image();
+taustakuvat.onload = () => {
+    console.log('Taustakuvien tiedosto ladattu.');
+}
+taustakuvat.src = '../kuvat/taustat.png';
+
+// Odotetaan että sivu on latautunut 
+window.onload = () => {
+    document.getElementById('odota').style.display = 'none';
+    document.getElementById('kanvaasi').style.opacity = 1;
+
+    animoi();
+}
+
+// Canvasin animointi
+function animoi(aika) {
+    // Kutsutaan tätä samaa funktiota ennen seuraavaa ruudun "maalausta"
+    window.requestAnimationFrame(animoi);
+    
+    // Koska ruudunpäivitysnopeus on yleensä 60Hz tai suurempi, rajoitetaan ruudun 
+    // piirtämistä n. 30 kertaan sekunnissa että animaatiot eivät pyöri liian nopeasti
+    let fps = Math.round(1000 / (aika-vanhaAika));
+    if (Number.isInteger(fps)) {
+        if (fps <= 30) { /* max. 30 fps */
+            vanhaAika = aika;
+            // Kaikki piirtäminen tämän jälkeen
+
+
+        }
+    }
+}
